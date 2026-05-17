21e0a42d057f81977d3c21c569b39ae3d490ce11
julybas <yaroslav@gmail.com>
2026-05-17 20:22:59 +0300

    added data for cards

---
+++ src/data/trains.js
@@ -0,0 +1,23 @@
+export const trains = [
+  {
+    id: 1,
+    number: "705K",
+    route: "Київ - Перемишль",
+    departure: "2026-05-20 17:00",
+    duration: "9 год 30 хв",
+  },
+  {
+    id: 2,
+    number: "091К",
+    route: "Київ - Львів",
+    departure: "2026-05-20 22:37",
+    duration: "7 год 20 хв",
+  },
+  {
+    id: 3,
+    number: "038Ш",
+    route: "Одеса - Київ",
+    departure: "2026-05-21 21:10",
+    duration: "10 год 15 хв",
+  },
+];
