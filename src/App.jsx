5c410102964bd9a440aff407ce1b7995ed143536
julybas <yaroslav@gmail.com>
2026-05-24 15:01:56 +0300

    added routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
+      <ToastContainer position="top-right" autoClose={3000} />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/booking/:trainId" element={<Booking />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
